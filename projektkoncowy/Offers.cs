624ffc2afbf3bd3cf49e177e94e77c8b43c0faed
KrakkuHedd0 <mtg23s55@gmail.com>
2023-01-24 20:43:38 +0100

    INIT

---
+++ projektkoncowy/Offers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace projektkoncowy
+{
+    internal class Offers
+    {
+
+        public void Off()
+        {
+            Login login = new Login();
+
+            Console.Write("Whats your name: ");
+            login.Name = Console.ReadLine();
+
+            Console.Write("Whats your surname: ");
+            login.Surname = Console.ReadLine();
+
+            Console.Write("what's is your education: ");
+            login.Ed = Console.ReadLine();
+
+            //offers
+
+            Offers offers = new Offers();
+
+           
+            Console.Write("chef doctor graphic_designer\n developer accountant trainer lawyer\n what offer you choose?: ");
+            string offer = Console.ReadLine();
+
+            Console.WriteLine("Great mate you choose");
+            Console.WriteLine("Your Name is:" + login.Name);
+            Console.WriteLine("Your Surname: " + login.Surname);
+            Console.WriteLine("Education: " + login.Ed);
+            Console.WriteLine("Offer: " + offer);
+            Console.WriteLine("the work as " + offer + " is waiting for you!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ projektkoncowy/works.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektkoncowy
+{
+    internal class works
+    {
+        private string chef = "chef";
+        private string doctor = "doctor";
+        private string graphic_designer = "graphic";
+        private string developer = "developer";
+        private string accountant = "accountant";
+        private string trainer = "trainer";
+        private string lawyer = "lawyer";
+
+
+        public string Offer(string offers)
+        {
+            if (offers == chef)
+            {
+                return chef;
+            }
+
+            if (offers == doctor)
+            {
+                return doctor;
+            }
+
+            if (offers == graphic_designer)
+            { 
+                return graphic_designer;
+            }
+
+            if (offers == developer)
+            {
+                return developer;
+            }
+
+            if (offers == accountant)
+            {
+                return accountant;
+            }
+
+            if (offers == trainer)
+            {
+                return trainer;
+            }
+
+            if (offers == lawyer)
+            {
+                return lawyer;
+            }
+
+            else 
+            {
+                return "there is no such offer";
+            }
+
+
+        }
+        
+    }
+}
